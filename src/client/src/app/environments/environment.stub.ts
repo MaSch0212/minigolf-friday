a45a0fe3449c21b1872e8545afb08acae9e3ce8a
Marc Schmidt <code@masch212.de>
2024-06-09 16:10:58 +0200

    chore: update npm packages

--- src/client/src/app/environments/environment.stub.ts
+++
@@ -1,7 +0,0 @@
-import { Environment } from './environment.type';
-import { provideStubs } from '../stubs';
-
-export const environment: Environment = {
-  getProviders: () => [provideStubs()],
-  authenticationRequired: false,
-};
