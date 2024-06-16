d4aaa1e756436301b94a9e7aa1da117716978fa9
André Schmidt <9435005+AnSch1510@users.noreply.github.com>
2024-06-16 22:25:17 +0200

    add commit to existing test and create new test without commit expecting zero events for the user

--- test/MinigolfFriday.IntegrationTests/Endpoints/Events/GetPlayerEventsTests.cs
+++ test/MinigolfFriday.IntegrationTests/Endpoints/Events/GetPlayerEventsTests.cs
@@ -13,6 +13,7 @@ public class GetPlayerEventsTests
         var events = await sut.Event()
             .WithTimeslot(map.Id)
             .WithTimeslot(map.Id, x => x.AllowFallback())
+            .WithCommitEvent()
             .BuildAsync(3);
 
         var response = await user.CallApi(x => x.GetPlayerEventsAsync(null, null));
@@ -21,6 +22,26 @@ public class GetPlayerEventsTests
         response
             .Events.Should()
             .BeEquivalentTo(ToPlayerEvents(events).Reverse(), o => o.WithStrictOrdering());
+    }
+
+    [TestMethod]
+    [DatabaseProviderDataSource]
+    public async Task GetPlayerEvents_Multiple_Not_Commited(DatabaseProvider databaseProvider)
+    {
+        await using var sut = await Sut.CreateAsync(databaseProvider);
+        var user = await sut.User().BuildAsync();
+        var map = await sut.MinigolfMap().BuildAsync();
+        var events = await sut.Event()
+            .WithTimeslot(map.Id)
+            .WithTimeslot(map.Id, x => x.AllowFallback())
+            .BuildAsync(3);
+
+        var playerEvents = await sut.Event().BuildAsync(0);
+
+        var response = await user.CallApi(x => x.GetPlayerEventsAsync(null, null));
+
+        response.Continuation.Should().BeNull();
+        response.Events.Should().BeEquivalentTo(ToPlayerEvents(playerEvents));
     }
 
     private static IEnumerable<PlayerEvent> ToPlayerEvents(IEnumerable<Event> events)
