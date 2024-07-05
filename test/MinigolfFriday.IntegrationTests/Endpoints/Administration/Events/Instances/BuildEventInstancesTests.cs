58804a5438e152208a4306daaffae3b83ea468fe
Marc Schmidt <github@masch212.de>
2024-07-05 08:54:39 +0200

    fix: timeslots should take place when 3 or more players are registered (#119)

--- test/MinigolfFriday.IntegrationTests/Endpoints/Administration/Events/Instances/BuildEventInstancesTests.cs
+++ test/MinigolfFriday.IntegrationTests/Endpoints/Administration/Events/Instances/BuildEventInstancesTests.cs
@@ -84,6 +84,133 @@ public partial class BuildEventInstancesTests
         (await sut.AppClient.GetEventAsync(@event.Id)).Event.Should().BeEquivalentTo(@event);
     }
 
+    [TestMethod]
+    [DatabaseProviderDataSource]
+    public async Task BuildEventInstances_WithFallback_BarelyEnoughPlayers_NoFallback(
+        DatabaseProvider databaseProvider
+    )
+    {
+        await using var sut = await Sut.CreateAsync(databaseProvider);
+        var map = await sut.MinigolfMap().BuildAsync();
+        var users = await sut.User().BuildAsync(6);
+        var @event = await sut.Event()
+            .WithRegistrationDeadline(DateTime.Now.AddHours(1))
+            .WithTimeslot(map.Id, x => x.AllowFallback())
+            .WithTimeslot(map.Id)
+            .BuildAsync();
+        foreach (var user in users.Take(3))
+        {
+            await user.CallApi(x =>
+                x.UpdatePlayerEventRegistrationsAsync(
+                    @event.Id,
+                    new()
+                    {
+                        TimeslotRegistrations =
+                        [
+                            new()
+                            {
+                                TimeslotId = @event.Timeslots.First().Id,
+                                FallbackTimeslotId = @event.Timeslots.ElementAt(1).Id
+                            }
+                        ]
+                    }
+                )
+            );
+        }
+        foreach (var user in users.Skip(3))
+        {
+            await user.CallApi(x =>
+                x.UpdatePlayerEventRegistrationsAsync(
+                    @event.Id,
+                    new()
+                    {
+                        TimeslotRegistrations =
+                        [
+                            new() { TimeslotId = @event.Timeslots.ElementAt(1).Id }
+                        ]
+                    }
+                )
+            );
+        }
+        @event.RegistrationDeadline = DateTime.Now;
+        await sut.AppClient.SetRegistrationDeadline(@event.Id, @event.RegistrationDeadline);
+
+        var instances = (await sut.AppClient.BuildEventInstancesAsync(@event.Id)).Instances;
+        instances
+            .First(x => x.TimeslotId == @event.Timeslots.First().Id)
+            .Instances.Single()
+            .PlayerIds.Should()
+            .BeEquivalentTo(users.Take(3).Select(x => x.User.Id));
+        instances
+            .First(x => x.TimeslotId == @event.Timeslots.ElementAt(1).Id)
+            .Instances.Single()
+            .PlayerIds.Should()
+            .BeEquivalentTo(users.Skip(3).Select(x => x.User.Id));
+    }
+
+    [TestMethod]
+    [DatabaseProviderDataSource]
+    public async Task BuildEventInstances_WithFallback_NotEnoughPlayers_UseFallback(
+        DatabaseProvider databaseProvider
+    )
+    {
+        await using var sut = await Sut.CreateAsync(databaseProvider);
+        var map = await sut.MinigolfMap().BuildAsync();
+        var users = await sut.User().BuildAsync(5);
+        var @event = await sut.Event()
+            .WithRegistrationDeadline(DateTime.Now.AddHours(1))
+            .WithTimeslot(map.Id, x => x.AllowFallback())
+            .WithTimeslot(map.Id)
+            .BuildAsync();
+        foreach (var user in users.Take(2))
+        {
+            await user.CallApi(x =>
+                x.UpdatePlayerEventRegistrationsAsync(
+                    @event.Id,
+                    new()
+                    {
+                        TimeslotRegistrations =
+                        [
+                            new()
+                            {
+                                TimeslotId = @event.Timeslots.First().Id,
+                                FallbackTimeslotId = @event.Timeslots.ElementAt(1).Id
+                            }
+                        ]
+                    }
+                )
+            );
+        }
+        foreach (var user in users.Skip(2))
+        {
+            await user.CallApi(x =>
+                x.UpdatePlayerEventRegistrationsAsync(
+                    @event.Id,
+                    new()
+                    {
+                        TimeslotRegistrations =
+                        [
+                            new() { TimeslotId = @event.Timeslots.ElementAt(1).Id }
+                        ]
+                    }
+                )
+            );
+        }
+        @event.RegistrationDeadline = DateTime.Now;
+        await sut.AppClient.SetRegistrationDeadline(@event.Id, @event.RegistrationDeadline);
+
+        var instances = (await sut.AppClient.BuildEventInstancesAsync(@event.Id)).Instances;
+        instances
+            .First(x => x.TimeslotId == @event.Timeslots.First().Id)
+            .Instances.Should()
+            .BeEmpty();
+        instances
+            .First(x => x.TimeslotId == @event.Timeslots.ElementAt(1).Id)
+            .Instances.Single()
+            .PlayerIds.Should()
+            .BeEquivalentTo(users.Select(x => x.User.Id));
+    }
+
     [GeneratedRegex(@"^\[.+\]\.Instances\[.+\]\.(GroupCode|Id)$")]
     private static partial Regex InstancesIgnoreRegex();
 }
